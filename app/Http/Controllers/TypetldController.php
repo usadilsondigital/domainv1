9f5c9a68e5dd1d03163b5c2750080f0e7aec268e
Adilson <usadilsondigital@gmail.com>
2023-12-23 00:11:27 -0600

    Typetld Controller

---
+++ app/Http/Controllers/TypetldController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\typetld;
+use App\Http\Requests\StoretypetldRequest;
+use App\Http\Requests\UpdatetypetldRequest;
+
+class TypetldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoretypetldRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(typetld $typetld)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(typetld $typetld)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatetypetldRequest $request, typetld $typetld)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(typetld $typetld)
+    {
+        //
+    }
+}
