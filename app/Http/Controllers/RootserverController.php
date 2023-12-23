88da48af51e5c0f870b3408888f555a17f5d8262
Adilson <usadilsondigital@gmail.com>
2023-12-23 00:10:47 -0600

    Rootserver Controller

---
+++ app/Http/Controllers/RootserverController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\rootserver;
+use App\Http\Requests\StorerootserverRequest;
+use App\Http\Requests\UpdaterootserverRequest;
+
+class RootserverController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorerootserverRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(rootserver $rootserver)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(rootserver $rootserver)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdaterootserverRequest $request, rootserver $rootserver)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(rootserver $rootserver)
+    {
+        //
+    }
+}
