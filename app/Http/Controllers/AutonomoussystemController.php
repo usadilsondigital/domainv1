45e81a59e777c690f7db6aa0a8b346443fc8cc4f
Adilson <usadilsondigital@gmail.com>
2023-12-23 00:44:26 -0600

    AutonomoussystemController

---
+++ app/Http/Controllers/AutonomoussystemController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\autonomoussystem;
+use App\Http\Requests\StoreautonomoussystemRequest;
+use App\Http\Requests\UpdateautonomoussystemRequest;
+
+class AutonomoussystemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreautonomoussystemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(autonomoussystem $autonomoussystem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(autonomoussystem $autonomoussystem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateautonomoussystemRequest $request, autonomoussystem $autonomoussystem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(autonomoussystem $autonomoussystem)
+    {
+        //
+    }
+}
