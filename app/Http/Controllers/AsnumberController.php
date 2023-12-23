0d7b66c97f9b5641f04916089f591fc1c8fb2886
Adilson <usadilsondigital@gmail.com>
2023-12-23 00:44:06 -0600

    AsnumberController

---
+++ app/Http/Controllers/AsnumberController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\asnumber;
+use App\Http\Requests\StoreasnumberRequest;
+use App\Http\Requests\UpdateasnumberRequest;
+
+class AsnumberController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreasnumberRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(asnumber $asnumber)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(asnumber $asnumber)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateasnumberRequest $request, asnumber $asnumber)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(asnumber $asnumber)
+    {
+        //
+    }
+}
