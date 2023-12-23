fa44c53260102698240fe43e1a265847cdb592aa
Adilson <usadilsondigital@gmail.com>
2023-12-23 00:11:43 -0600

    Unsystem Controller

---
+++ app/Http/Controllers/UnsystemController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\unsystem;
+use App\Http\Requests\StoreunsystemRequest;
+use App\Http\Requests\UpdateunsystemRequest;
+
+class UnsystemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreunsystemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(unsystem $unsystem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(unsystem $unsystem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateunsystemRequest $request, unsystem $unsystem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(unsystem $unsystem)
+    {
+        //
+    }
+}
