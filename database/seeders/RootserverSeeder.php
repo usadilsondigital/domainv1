343d8b786080242c997313540bc80736653eac29
Adilson <usadilsondigital@gmail.com>
2023-12-23 00:08:27 -0600

    RootServer Seeder

---
+++ database/seeders/RootserverSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RootserverSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
