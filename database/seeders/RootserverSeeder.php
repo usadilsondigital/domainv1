1d30d7d38d712d11adf3ade7a028c49a11175e02
Adilson <usadilsondigital@gmail.com>
2023-12-25 22:11:15 -0600

    RootServer Seeder

--- database/seeders/RootserverSeeder.php
+++ database/seeders/RootserverSeeder.php
@@ -12,6 +12,20 @@ class RootserverSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        //  Letter	IPv4 address	IPv6 address	Old name	Operator Software countryid        
+        $file = fopen('rootserverseeder.csv', 'r');
+        while (($line = fgetcsv($file)) !== FALSE) {
+               \DB::table('rootservers')->insert([
+                'letter'=> $line[0],
+                'ipv4'=> $line[1],
+                'ipv6'=> $line[2],
+                'oldname'=> $line[3],
+                'operator'=> $line[4],
+                'software'=> $line[5],
+                'countryid'=> $line[6]
+            ]);
+        }
+        
+        fclose($file);
     }
 }
