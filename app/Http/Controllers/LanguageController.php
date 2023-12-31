74c898428990fd929d7acb793c357a0ccdb570ad
Adilson <usadilsondigital@gmail.com>
2023-12-31 17:55:45 -0600

    Language Controller

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\language;
+use App\Http\Requests\StorelanguageRequest;
+use App\Http\Requests\UpdatelanguageRequest;
+
+class LanguageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorelanguageRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(language $language)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(language $language)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatelanguageRequest $request, language $language)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(language $language)
+    {
+        //
+    }
+}
