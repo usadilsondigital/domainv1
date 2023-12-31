64cc767ed663f49d7c95dfe13a2263b2377794bd
Adilson <usadilsondigital@gmail.com>
2023-12-31 17:55:30 -0600

    Internet TLD Controller

---
+++ app/Http/Controllers/InternettldController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\internettld;
+use App\Http\Requests\StoreinternettldRequest;
+use App\Http\Requests\UpdateinternettldRequest;
+
+class InternettldController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreinternettldRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(internettld $internettld)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(internettld $internettld)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateinternettldRequest $request, internettld $internettld)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(internettld $internettld)
+    {
+        //
+    }
+}
