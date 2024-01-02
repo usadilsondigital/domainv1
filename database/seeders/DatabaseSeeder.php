4fe45f584dc23bdf27b35178b4b6a8eac32e6c04
Adilson <usadilsondigital@gmail.com>
2024-01-01 23:51:33 -0600

    database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,12 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\RootserverSeeder;
+use Database\Seeders\CountrySeeder;
+use Database\Seeders\UnsystemSeeder;
+use Database\Seeders\AsnumberSeeder;
+use Database\Seeders\AutonomoussystemSeeder;
+use Database\Seeders\TypetldSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +24,16 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        //flow of app
+
+        $this->call([
+                // UnsystemSeeder::class,
+                //CountrySeeder::class,
+                //AutonomoussystemSeeder::class,
+                //RootserverSeeder::class,
+                //AsnumberSeeder::class,
+            TypetldSeeder::class
+        ]);
     }
 }
