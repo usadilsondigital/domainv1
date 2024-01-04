5b5864dfa776b3e0145ba0d99cbdb583cfa8f134
Adilson <usadilsondigital@gmail.com>
2024-01-03 22:22:20 -0600

    addig name,count, to words

--- database/migrations/2024_01_04_025220_create_words_table.php
+++ database/migrations/2024_01_04_025220_create_words_table.php
@@ -13,6 +13,8 @@ return new class extends Migration
     {
         Schema::create('words', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->integer('characterscount');            
             $table->timestamps();
         });
     }
