3e74511a628975d333d7c5bac4f2e91d5415e84b
Adilson <usadilsondigital@gmail.com>
2024-01-29 13:08:23 -0600

    create Routing to Index consumer

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AutonomoussystemController;
+use App\Http\Controllers\ConsumerController;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,4 +22,4 @@ Route::get('/', function () {
 
 Route::get('/as', [AutonomoussystemController::class, 'index']);
 
-Route::get('/consume', [AutonomoussystemController::class, 'index']);
+Route::get('/consumer', [ConsumerController::class, 'index']);
