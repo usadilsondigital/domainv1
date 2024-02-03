a296542df6680c0e4335aee6f1fd97805212b638
Adilson <usadilsondigital@gmail.com>
2024-02-02 18:30:22 -0600

    updating consumer methods

--- app/Http/Controllers/ConsumerController.php
+++ app/Http/Controllers/ConsumerController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Crypt;
 use Illuminate\Contracts\Encryption\DecryptException;
+use Illuminate\Support\Facades\DB;
 
 class ConsumerController extends Controller
 {
@@ -17,8 +18,8 @@ class ConsumerController extends Controller
 
 
     }
-    
-   
+
+
 
     /**
      * Show the form for creating a new resource.
@@ -86,36 +87,63 @@ class ConsumerController extends Controller
     }
 
 
-     /**
+    /**
      * Searcher .
      */
     public function searcher()
     {
 
 
-      //grab first word > 3  letters
-      //looping in the extensions
-      //grab first extension create "word.extension"
-      //try to access to this "https://www.whois.com/whois/"+"word.extension"
+        //grab first word > 3  letters
+        //looping in the extensions
+        //grab first extension create "word.extension"
+        //try to access to this "https://www.whois.com/whois/"+"word.extension"
 
-      $word  = $this->wordMoreThreeLetters();
-      $extension = $this->extensionNow();
-      $cadena =  $word . $extension;
-      $urlToTest = "https://www.whois.com/whois/".$cadena;
-      $response = Http::get($urlToTest);
-      dd($response);
+        $word = $this->wordMoreThreeLetters();
+        dd($word);
+        $extension = $this->extensionNow();
+        $cadena = $word . $extension;
+        $urlToTest = "https://www.whois.com/whois/" . $cadena;
+        $response = Http::get($urlToTest);        
+        $body = $response->body();
+        $quantity = substr_count($body,"whois-data");
+        if($quantity>2){
+            dd('NOT open');
+            //search other characteristcs
+            //process raw data or process tags
+            //see exmaple of response.txt
+            //$qua = substr_count($body,'class="df-raw"');
+            //$posRawData = strripos($body,'class="df-raw"');
+            //dd(strripos($body,'class="df-raw"'));
+            //$newPosRawData = $posRawData+ 34;           
+            //dd($body[$newPosRawData]);
+        }else{
+             dd('open');
+             $quant = substr_count($body,"Looks like this domain has not been registered yet");
+             dd($quant);
+        }
+        //dd($response->status());
+        
 
     }
 
 
-     /**
+    /**
      * wordMoreThreeLetters .
      */
     public function wordMoreThreeLetters()
     {
+       
+    }
 
 
-   
+    /**
+     * wordMoreThreeLetters .
+     */
+    public function extensionNow()
+    {
+        $extAux = ".com";
+        return $extAux;
     }
 
 
